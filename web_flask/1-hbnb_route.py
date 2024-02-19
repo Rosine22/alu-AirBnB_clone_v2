cbb14f1fbbbfd82aaa822d29def1436fadeb7170
Rosine22 <r.uwineza@alustudent.com>
2024-02-19 03:41:37 -0800

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
-"""
-    python script that starts a Flask web application
-"""
+"""A python script that starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
-    """
-        Return: string when route queried
-    """
+    """Display Hello HBNB! from the root route /"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-        Return: string when route queried
-    """
+    """Display HBNB from the route /hbnb"""
     return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
