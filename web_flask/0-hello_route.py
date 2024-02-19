84506fe1bea8b478a4e1355293e4cf30a11c0628
Rosine22 <r.uwineza@alustudent.com>
2024-02-19 03:43:29 -0800

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-    python script that starts a Flask web application
-"""
+"""A python script that starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_flask():
-    """
-        Return: string when route queried
-    """
+@app.route('/', strict_slashes=False)
+def hello():
+    """Display Hello HBNB! from the root route /"""
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
